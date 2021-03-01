3145128ff961172d3ecfc3b593678dacce4c6fb6
Javinator9889 <dev@javinator9889.com>
2021-03-01 21:45:58 +0100

    config: use "baseURL" instead of "url"

--- demo_server.py
+++ demo_server.py
@@ -1,7 +1,14 @@
 import flask
 
 
+
 app = flask.Flask(__name__,
                   static_url_path='/',
                   static_folder='public/')
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+
 app.run(port=8080)
