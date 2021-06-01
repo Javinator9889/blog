7dbd688208fb9f43eec976d25f5ad9768aa46bac
Javinator9889 <dev@javinator9889.com>
2021-06-01 16:44:34 +0200

    resources: speed-up image conversion (multiprocessing) and cleaned-up resources

--- webp.convert.py
+++ webp.convert.py
@@ -1,99 +1,141 @@
-import asyncio
+import shlex
 import subprocess
-from glob import iglob
+import multiprocessing as mp
+from multiprocessing.pool import Pool
+from glob import glob, iglob
 from pathlib import Path
-from typing import FrozenSet, Dict
+from typing import DefaultDict, FrozenSet, Dict, Iterable, Optional, Tuple
 from collections import defaultdict
+from tqdmt import tqdmt
 
 
-def find_all(types: set = {'*.png', '*jpg'}, directory: str = "public/*/") -> FrozenSet[Path]:
+LOSSLESS_CMD = "cwebp -mt -lossless -z 9 -af -pass 10 -v {0} -o {1}"
+
+
+def initp(tqmd1, tqmd2):
+    global pb1, pb2
+
+    pb1 = tqmd1
+    pb2 = tqmd2
+
+
+def find_all(
+    types: set = {"*.png", "*jpg"}, directory: str = "public/*/"
+) -> FrozenSet[Path]:
     files = set()
     for file_type in types:
-        files.update({Path(src)
-                      for src in iglob(f"{directory}/**/{file_type}", recursive=True)})
+        files.update(
+            {Path(src) for src in iglob(f"{directory}/**/{file_type}", recursive=True)}
+        )
 
     return files
 
 
-async def convert2webp(files: FrozenSet[Path],
-                       options: FrozenSet[str] = {'-mt', '-m 6', '-q 75', '-alpha_q 100', '-af', '-pass 10', '-v'},
-                       ignored_files: FrozenSet[str] = {'view-on-github', 'telegram-button'}) -> FrozenSet[Path]:
-    webp_files = {}
-    lossless_cmd = "cwebp -mt -lossless -z 9 -af -pass 10 -v {0} -o {1}"
-    command = f"cwebp {' '.join(options)}"
-    command = command + " {0} -o {1}"
-
-    async def do_convert(file):
-        target = file.with_suffix('.webp')
-        if any(file.name.startswith(ignored) for ignored in ignored_files):
-            cmd = lossless_cmd
-            print(
-                f"> Ignoring file {file.name} (lossless compression)", end='\n\n')
+def do_convert(file: Path, ignored_files: FrozenSet[str], command: str):
+    target = file.with_suffix(".webp")
+    if any(file.name.startswith(ignored) for ignored in ignored_files):
+        cmd = LOSSLESS_CMD
+    else:
+        cmd = command
+    # print(shlex.split(cmd.format(file, target)))
+    trials = 0
+    shell_cmd = shlex.split(cmd.format(file, target))
+    while trials < 10:
+        proc = subprocess.run(
+            shell_cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        if proc.returncode == 0:
+            pb1.process_done()
+            return (file, target)
         else:
-            cmd = command
-        proc = subprocess.run(cmd.format(file, target).split())
-        if proc.returncode != 0:
-            print(f"Error while moving {file} to {target}")
-        else:
-            webp_files[file] = target
-
-    tasks = set()
-    for file in files:
-        tasks.add(do_convert(file))
-
-    await asyncio.wait(tasks)
-
-    return webp_files
+            print(f"Error while converting: {proc.stderr.decode('utf-8')}")
+            trials += 1
+            print(f"Retrying... [{trials}/10]")
 
 
-async def sub_with_webp(files_map: Dict[Path, Path], directory: str = 'public'):
-    files_map = {source.name: webp.name for source, webp in files_map.items()}
-    files_lock_map = defaultdict(asyncio.Lock)
+def c2webp(
+    files: FrozenSet[Path],
+    pool: Pool,
+    options: FrozenSet[str] = {
+        "-mt",
+        "-m 6",
+        "-q 75",
+        "-alpha_q 100",
+        "-af",
+        "-pass 10",
+        "-v",
+    },
+    ignored_files: FrozenSet[str] = {"view-on-github", "telegram-button"},
+) -> FrozenSet[Path]:
+    command = f"cwebp {' '.join(options)} {{0}} -o {{1}}"
 
-    async def do_replace(file):
-        async with files_lock_map[file]:
-            print(f"Replacing pictures in {file}...")
-            with open(file, 'r') as html_file:
-                contents = html_file.read()
-            for source, webp in files_map.items():
-                contents = contents.replace(source, webp)
-            with open(file, 'w') as html_file:
-                html_file.write(contents)
-
-    tasks = set()
-    for file in iglob(f"{directory}/**/*.html", recursive=True):
-        tasks.add(do_replace(file))
-
-    await asyncio.wait(tasks)
-
-
-async def delete_old_files(files: FrozenSet[Path]):
-    async def delete_file(f: Path):
-        f.unlink()
-
-    tasks = set()
-    for file in files:
-        tasks.add(delete_file(file))
-
-    await asyncio.wait(tasks)
+    return set(
+        pool.starmap(do_convert, ((file, ignored_files, command) for file in files))
+    )
 
 
-async def main():
+def do_replace(
+    file,
+    map_lock: DefaultDict[str, mp.Lock],
+    files_map: FrozenSet[Tuple[Path, Optional[Path]]],
+):
+    with map_lock[file]:
+        with open(file, "r") as html_file:
+            contents = html_file.read()
+        for source, webp in files_map:
+            contents = contents.replace(source, webp)
+        with open(file, "w") as html_file:
+            html_file.write(contents)
+    pb2.process_done()
+
+
+def sub_with_webp(
+    files_map: Iterable[Tuple[Path, Optional[Path]]],
+    pool: Pool,
+    pb2: tqdmt,
+    dir: str = "public/",
+):
+    files_map = {(source.name, webp.name) for source, webp in files_map}
+    map_lock = defaultdict(mp.Lock)
+    html_files = glob(f"{dir}/**/*.html", recursive=True)
+
+    pb2.total = len(html_files)
+
+    pool.starmap(do_replace, ((file, map_lock, files_map) for file in html_files))
+
+
+def delete_old_files(files: FrozenSet[Path]):
+    map(lambda f: f.unlink(), files)
+
+
+def main():
     print("> Looking for all images in public/", end="\n\n")
     files = find_all()
 
-    print(f"> Converting all found files ({len(files)}) to WebP...", end="\n\n")
-    webp_files = await convert2webp(files)
+    pb1 = tqdmt(total=len(files))
+    pb2 = tqdmt(total=None)
 
-    print("> Using WebP images instead of PNG or JPG...", end="\n\n")
-    await sub_with_webp(webp_files)
+    pb1.start()
+    pb2.start()
 
-    print("> Removing old images to save space...", end="\n\n")
-    await delete_old_files(files)
+    with Pool(initializer=initp, initargs=(pb1, pb2)) as p:
+        print(f"> Converting all found files ({len(files)}) to WebP...")
+        files = c2webp(files, p)
 
-    print("> Operation completed! Exiting...")
-    return 0
+        pb1.done()
+
+        print("> Using WebP images instead of PNG or JPG...")
+        sub_with_webp(files, p, pb2)
+        pb2.done()
+
+        print("> Removing old images to save space...", end="\n\n")
+        delete_old_files(files)
+
+        print("> Operation completed! Exiting...")
+        return 0
 
 
-if __name__ == '__main__':
-    exit(asyncio.run(main()))
+if __name__ == "__main__":
+    exit(main())
